4d00e4470a24cafce95d87cf5c86ca709d61b4bf
ethanhan2014 <laudy2012@gmail.com>
2022-02-03 23:48:07 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+## PCTF project Team 4
+## Team member: 
+## Description
+#  Network Defense tool
+##
+from scapy.all import *
+
+
+class NetworkTool:
+
+    def __init__(self):
+        pass
+
+    def pkt_callback(self, pkt):
+        pass
+
+    def firewall_setup():
+        pass
+
+    def run(self):
+        pass
+
+def main():
+    NetworkTool().run()
+
+if __name__ == '__main__':
+    main()
+
