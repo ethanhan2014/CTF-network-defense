33eaab995a93b2c8a2bb9a47a9f4d4af560977e0
ethanhan2014 <laudy2012@gmail.com>
2022-02-17 22:10:48 -0500

    add main arguments; write to pcap

--- NetworkTool.py
+++ NetworkTool.py
@@ -8,10 +8,13 @@ Description: Packet Sniffing and processing
 
 class NetworkTool:
 
-    def __init__(self, iface):
+    def __init__(self, iface, pcapfile):
         self.iface = iface
+        self.pcapfile = pcapfile
 
     def pkt_callback(self, pkt):
+        "write pkt into pcap file"
+        wrpcap(self.pcapfile, pkt, append=True)
         pass
 
     def run(self):
--- main.py
+++ main.py
@@ -10,14 +10,23 @@ Description:
 '''
 
 from scapy.all import *
+import argparse
 import iptc
 from PacketPayloadAnalyzer import *
 from PacketPayloadEngine import *
 import NetworkTool
 
 def main():
-    print("Firewall starting up...")
-    NetworkTool().run()
+    parser = argparse.ArgumentParser(description="NetworkDefenseTool")
+    parser.add_argument(
+        '--interface', help="listening network interface", dest='interface', required=True)
+    parser.add_argument(
+        '--pcapfile', help="output pcap file path", dest='pcapfile', required=True)
+    args = parser.parse_args()
+    interface = str(args.interface)
+    pcapfile = str(args.pcapfile)
+    print("Starting up Network Defense Tool...")
+    NetworkTool(interface, pcapfile).run()
     #testSuite = PacketPayloadAnalyzer_TestSuite()
 
 if __name__ == '__main__':
